2477b2622f165024d84b854296c9f58bdaeda183
lizhanpeng <lizhanpeng@zego.im>
2019-11-10 22:43:45 +0800

    android example release.

---
+++ GetAppIDConfig.java
@@ -0,0 +1,21 @@
+package com.zego.common;
+
+/**
+ * Created by zego on 2018/11/15.
+ */
+
+public class GetAppIDConfig {
+
+    /**
+     * 请提前在即构管理控制台获取 appID 与 appSign
+     *  AppID 填写样式示例：
+            public static final long appID = 123456789L;
+     *  appSign 填写样式示例：
+            public static final String appSign = "1e2a3c.....f8";
+     *
+     */
+
+    public static final long appID = ;
+
+    public static final String appSign = ;
+}
---
+++ ZegoExpressExample/common/src/main/java/com/zego/common/GetAppIDConfig.java
@@ -0,0 +1,21 @@
+package com.zego.common;
+
+/**
+ * Created by zego on 2018/11/15.
+ */
+
+public class GetAppIDConfig {
+
+    /**
+     * 请提前在即构管理控制台获取 appID 与 appSign
+     *  AppID 填写样式示例：
+            public static final long appID = 123456789L;
+     *  appSign 填写样式示例：
+            public static final String appSign = "1e2a3c.....f8";
+     *
+     */
+
+    public static final long appID = 1739272706L;
+
+    public static final String appSign = "1ec3f85cb2f21370264eb371c8c65ca37fa33b9defef2a85e0c899ae82c0f6f8";
+}
