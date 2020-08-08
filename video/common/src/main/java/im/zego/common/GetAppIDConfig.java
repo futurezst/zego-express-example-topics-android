0641690206002f54f1e372ca8907c5cda5b998eb
Patrick Fu <patrickfu@zego.im>
2020-08-08 19:35:22 +0800

    docs: Update README

--- video/common/src/main/java/im/zego/common/GetAppIDConfig.java
+++ video/common/src/main/java/im/zego/common/GetAppIDConfig.java
@@ -6,9 +6,9 @@ public class GetAppIDConfig {
 
     // for example:
     //     public static final long appID = 123456789L;
-    public static final long appID = ;
+    public static final long appID = 1739272706L;
 
     // for example:
     //    public static final String appSign = "0123456789012345678901234567890123456789012345678901234567890123";
-    public static final String appSign = ;
+    public static final String appSign = "1ec3f85cb2f21370264eb371c8c65ca37fa33b9defef2a85e0c899ae82c0f6f8";
 }
